f5b560d7a88e733730179f31ac047716ccff0ea2
Luke <97401100+LukeBlack952@users.noreply.github.com>
2022-01-09 14:14:28 -0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,23 @@
+
+-- Made by LukeBlack952/LosProTroll (GitHub/Roblox)
+
+-- Variables
+local ZareX = Instance.new("ScreenGui-")
+local Membership = Instance.new("StringValue")
+local DragGUI = Instance.new("LocalScript")
+local GUI = Instance.new("Frame")
+local SHGUI = Instance.new("Frame")
+local Open = Instance.new("TextButton")
+
+-- Setting Up Starter Gui
+ZareX.Name = "ZareX"
+ZareX.Parent = game.StarterGui
+ZareX.ResetOnSpawn = false
+
+-- Setting Up Membership
+Membership.Name = "Membership"
+Membership.Parent = ZareX
+Membership.Value = "Premium"
+
+
+DragGUI.Parent = ZareX
