6e07ee554279c0f020949f504cf9fc89180c9bdc
Luke <97401100+LukeBlack952@users.noreply.github.com>
2022-01-09 16:55:03 -0300

    Update Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -1,10 +1,10 @@
 
 -- Made by LukeBlack952/LosProTroll (GitHub/Roblox)
+-- How to load on a Script Executor: Type "loadstring()"
 
 -- Variables
 local ZareX = Instance.new("ScreenGui-")
 local Membership = Instance.new("StringValue")
-local DragGUI = Instance.new("LocalScript")
 local GUI = Instance.new("Frame")
 local SHGUI = Instance.new("Frame")
 local Open = Instance.new("TextButton")
@@ -19,5 +19,19 @@ Membership.Name = "Membership"
 Membership.Parent = ZareX
 Membership.Value = "Premium"
 
+-- Setting Up GUI
+GUI.Parent = ZareX
+GUI.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+GUI.BorderColor3 = Color3.fromRGB(0, 0, 0)
+GUI.BorderSizePixel = 0
+GUI.Position = UDim2.new(-0.001, 0, 0.301, 0)
+GUI.Size = UDim2.new(0, 375, 0, 235)
 
-DragGUI.Parent = ZareX
+
+-- Setting Up SHGUI
+SHGUI.Parent = ZareX
+SHGUI.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+SHGUI.BorderColor3 = Color3.fromRGB(0, 0, 0)
+SHGUI.BorderSizePixel = 0
+SHGUI.Position = UDim2.new(-0.001, 0, 0.301, 0)
+SHGUI.Size = UDim2.new(0, 375, 0, 235)
