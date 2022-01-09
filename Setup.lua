dc053809338a8a839e31c7cac67b69ffe65d99e8
Luke <97401100+LukeBlack952@users.noreply.github.com>
2022-01-09 17:24:36 -0300

    Update Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -3,7 +3,7 @@
 -- How to load on a Script Executor: Type "loadstring()"
 
 -- Variables
-local ZareX = Instance.new("ScreenGui-")
+local ZareX = Instance.new("ScreenGui")
 local Membership = Instance.new("StringValue")
 local GUI = Instance.new("Frame")
 local SHGUI = Instance.new("Frame")
@@ -13,6 +13,7 @@ local Open = Instance.new("TextButton")
 ZareX.Name = "ZareX"
 ZareX.Parent = game.StarterGui
 ZareX.ResetOnSpawn = false
+ZareX.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 -- Setting Up Membership
 Membership.Name = "Membership"
@@ -30,8 +31,20 @@ GUI.Size = UDim2.new(0, 375, 0, 235)
 
 -- Setting Up SHGUI
 SHGUI.Parent = ZareX
-SHGUI.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+SHGUI.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
 SHGUI.BorderColor3 = Color3.fromRGB(0, 0, 0)
 SHGUI.BorderSizePixel = 0
-SHGUI.Position = UDim2.new(-0.001, 0, 0.301, 0)
-SHGUI.Size = UDim2.new(0, 375, 0, 235)
+SHGUI.Position = UDim2.new(0.67, 0, 0.33, 0)
+SHGUI.Size = UDim2.new(0, 300, 0, 220)
+
+-- Setting Up Open
+Open.Parent = ZareX
+Open.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+Open.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Open.BorderMode = Enum.BorderMode.Inset
+Open.BorderSizePixel = 1
+Open.Position = UDim2.new(0, 0, 0.958, 0)
+Open.Size = UDim2.new(0.054, 0, 0.042, 0)
+Open.Text = "Open ZareX"
+Open.TextColor3 = Color3.fromRGB(255, 255, 255)
+Open.TextScaled = true
